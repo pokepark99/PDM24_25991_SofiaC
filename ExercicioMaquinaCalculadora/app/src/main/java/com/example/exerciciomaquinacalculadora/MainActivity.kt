1adc5060331d1db15348916bf8e6fb31decdb6cd
Sofia <a25991@alunos.ipca.pt>
2024-10-12 16:06:02 +0100

    Optimizacao dos butoes. Ainda necessario desenvolver as operacoes

--- ExercicioMaquinaCalculadora/app/src/main/java/com/example/exerciciomaquinacalculadora/MainActivity.kt
+++ ExercicioMaquinaCalculadora/app/src/main/java/com/example/exerciciomaquinacalculadora/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,79 +45,70 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun Calc() {
+    var apr = remember {//valor a ser apresentado na tela
+        mutableStateOf("") //empty string
+    }
+    var valorAnterior = remember {//valor inserido anteriormente
+        mutableStateOf("")
+    }
+    var operacao = remember {//valor do resultado atual
+        mutableStateOf("")
+    }
+
+    val listaButoes = listOf( //usar val pq isto nao pode ser mudado
+        listOf("sqrt", "%", "+/-", "CE"),
+        listOf("7", "8", "9", "%"),
+        listOf("4", "5", "6", "x"),
+        listOf("1", "2", "3", "-"),
+        listOf("0", ".", "=", "+")
+    )
+    val listaOperadors = listOf("/", "*", "-", "+")
+
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Row {
-            TextField(value = "", label = { Text("calc") }, onValueChange = {})
+            TextField(value = apr.value, label = { Text("") }, onValueChange = {})
         }
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Button(onClick = {}) {
-                Text("7")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text("8")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text("9")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text("/")
-            }
-        }
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Button(onClick = {}) {
-                Text("4")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text("5")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text("6")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text("*")
-            }
-        }
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Button(onClick = {}) {
-                Text("1")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text("2")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text("3")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text("-")
-            }
-        }
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Button(onClick = {}) {
-                Text("0")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text(".")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text("=")
-            }
-            Spacer(modifier = Modifier.width(3.dp))
-            Button(onClick = {}) {
-                Text("+")
+        Butoes(listaButoes, apr)
+    }
+}
+
+@Composable
+fun Butoes(listaButoes: List<List<String>>, apr: MutableState<String>){
+    for (lista in listaButoes){
+        Row(verticalAlignment = Alignment.CenterVertically){
+            for(numero in lista){
+                Button(onClick={
+                    if(numero =="+/-"){
+                        if (apr.value.startsWith("-")) {
+                            apr.value = apr.value.drop(1)
+                        } else {
+                            apr.value = "-${apr.value}"
+                        }
+                    } else if(apr.value ==""){
+                        if(numero =="."){
+                            apr.value = "0${apr.value}"
+                            apr.value += numero
+                        } else if (numero != "0"){
+                            apr.value += numero
+                        }
+                    } else if(numero =="."){
+                        if(!(apr.value.contains("."))) { //se nao tem . --> um numero nao pode ter mais que um ponto decimal
+                            apr.value += numero
+                        }
+                    } else {
+                        apr.value += numero
+                    }
+                }){
+                    Text(numero)
+                    }
+                Spacer(modifier = Modifier.width(3.dp))
             }
         }
     }
+}
+
+fun Resultado(){ //funcao para calcular o resultado
+
 }
 
 @Preview(showBackground = true)
